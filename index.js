1d85749cb1f289c00dc47267429d6e63a35b2b4d
Oliver Joseph Ash <oliverjash@icloud.com>
2013-10-16 23:18:42 +0100

    Take 2

--- index.js
+++ index.js
@@ -5,25 +5,40 @@ var define;
   
   var context = {};
 
-  function require(dependencies, cb) {
-    var dependencyModules = dependencies.reduce(function (modules, dependencyName) {
-      return modules.concat(context[dependencyName]);
-    }, []);
-    cb.apply(this, dependencyModules);
+  function resolve(dependencies) {
+    return dependencies.map(function (dependencyName) {
+      var module = context[dependencyName];
+      return module.cb.apply(this, resolve(module.dependencies));
+    });
+  }
+  
+  function require(dependencies, cb) {    
+    cb.apply(this, resolve(dependencies));
   }
   
   function define(name, dependencies, cb) {
-    context[name] = cb();
+    // Create the module object
+    context[name] = {
+      dependencies: dependencies,
+      cb: cb
+    };
   }
   
-  define('example', [], function () {
+  define('module-1', [], function () {
+    return {
+      baz: 'bar'
+    };
+  });
+  
+  define('module-2', [ 'module-1' ], function (module1) {
+    console.log('module-1', module1);
     return {
       foo: 'bar'
     };
   });
   
-  require([ 'example' ], function (example) {
-    console.log(example.foo);
+  require([ 'module-2' ], function (module2) {
+    console.log('module-2', module2);
   });
 
 })();
